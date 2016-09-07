d370ee5c93e31067ab94d3fb355463f5ddb2db2f
action jackson <action@localhost.localdomain>
2016-09-07 11:11:12 -0600

    trying to unbreak things

---
+++ src/classes/index.d.ts
@@ -0,0 +1,3 @@
+export * from './date-ordinal';
+export * from './date-math-expression';
+export * from './date-cruncher';
---
+++ src/enums.d.ts
@@ -0,0 +1,2 @@
+export * from './enums/days-of-the-week';
+export * from './enums/months-of-the-year';
